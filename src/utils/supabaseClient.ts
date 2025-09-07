e5081a1ed0d04637fe02bf97bcd63a2350a98d40
JeffLCTec <j.lopez.5@estudiantec.cr>
2025-09-07 04:54:15 -0600

    error

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Estas variables vienen de tu .env
-const supabaseUrl = import.meta.env.SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.SUPABASE_SERVICE_ROLE as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
